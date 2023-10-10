7330bd667a86f9319bf96108b241ebb9a4c4bb3d
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:39:01 +0300

    Paged list extensions

---
+++ src/ModsenBookLibrary.Application/Extensions/PagedListExtension.cs
@@ -0,0 +1,18 @@
+﻿using ModsenBookLibrary.Application.Models;
+
+namespace ModsenBookLibrary.Application.Extensions;
+public static class PagedListExtensions
+{
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> values, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+    {
+        return await PagedList<T>.CreateAsync(values, pageNumber, pageSize, cancellationToken);
+    }
+
+    public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> values, CancellationToken cancellationToken = default)
+    {
+        const int defaultPageNumber = 1;
+        const int defaultPageSize = 10;
+
+        return await values.ToPagedListAsync(defaultPageNumber, defaultPageSize, cancellationToken);
+    }
+}
