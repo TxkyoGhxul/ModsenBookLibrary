8277695ed03493c5e58875cc043adcaa3a16f8f5
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:36:36 +0300

    Custom exception

---
+++ src/ModsenBookLibrary.Domain/Exceptions/InvalidISBNException.cs
@@ -0,0 +1,14 @@
+﻿namespace ModsenBookLibrary.Domain.Exceptions;
+public class InvalidISBNException : Exception
+{
+    public InvalidISBNException() : base("Provide valid ISBN")
+    {
+    }
+
+    public InvalidISBNException(string isbnValue) : base($"Provide valid ISBN. Current value: {isbnValue}")
+    {
+        IsbnValue = isbnValue;
+    }
+
+    public string IsbnValue { get; init; }
+}
