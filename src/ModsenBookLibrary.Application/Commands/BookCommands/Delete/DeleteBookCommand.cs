e69a5ef4e9f51ca34f457070d0ef6b4619c24c7b
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:39:44 +0300

    Implemented delete book command

---
+++ src/ModsenBookLibrary.Application/Commands/BookCommands/Delete/DeleteBookCommand.cs
@@ -0,0 +1,5 @@
+﻿using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Commands.BookCommands.Delete;
+public record DeleteBookCommand(Guid Id) : IDeleteCommand<Book>;
---
+++ src/ModsenBookLibrary.Application/Commands/BookCommands/Delete/DeleteBookCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace ModsenBookLibrary.Application.Commands.BookCommands.Delete;
+
+public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+{
+    public DeleteBookCommandValidator()
+    {
+        RuleFor(b => b.Id).NotNull().NotEmpty().NotEqual(Guid.Empty);
+    }
+}
