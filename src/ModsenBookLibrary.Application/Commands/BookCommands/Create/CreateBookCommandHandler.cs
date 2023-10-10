280af0aacc1e0342df31ab994ece94322337111e
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:39:34 +0300

    Implemented create book command

---
+++ src/ModsenBookLibrary.Application/Commands/BookCommands/Create/CreateBookCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Application.Models;
+using ModsenBookLibrary.Domain.Exceptions;
+using ModsenBookLibrary.Domain.Models;
+using ModsenBookLibrary.Domain.Primitives;
+
+namespace ModsenBookLibrary.Application.Commands.BookCommands.Create;
+
+internal class CreateBookCommandHandler : BaseCommandHandler<Book>, ICreateCommandHandler<CreateBookCommand, Book>
+{
+    private readonly IRepository<Genre> _genreRepository;
+    private readonly IRepository<User> _userRepository;
+
+    public CreateBookCommandHandler(IRepository<Genre> genreRepository, IRepository<Book> repository, IUnitOfWork unitOfWork, IRepository<User> userRepository) : base(repository, unitOfWork)
+    {
+        _genreRepository = genreRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<Result<Book>> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+    {
+        var validISBN = ISBN.TryFrom(request.ISBN, out var isbn);
+        if (!validISBN)
+        {
+            return new InvalidISBNException(request.ISBN);
+        }
+
+        var genres = await _genreRepository.Get(g => request.GenresIds.Contains(g.Id)).ToListAsync(cancellationToken);
+        var authors = await _userRepository.Get(a => request.AuthorsIds.Contains(a.Id)).ToListAsync(cancellationToken);
+
+        var book = new Book(request.Name, request.Description, isbn, genres, authors);
+
+        _repository.Create(book);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return book;
+    }
+}
---
+++ src/ModsenBookLibrary.Application/Commands/BookCommands/Create/CreateBookCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace ModsenBookLibrary.Application.Commands.BookCommands.Create;
+
+public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
+{
+    public CreateBookCommandValidator()
+    {
+        RuleFor(c => c.Name).Length(3, 35);
+
+        RuleFor(c => c.Description).Length(3, 350);
+
+        RuleFor(c => c.ISBN).Length(10, 25);
+    }
+}
