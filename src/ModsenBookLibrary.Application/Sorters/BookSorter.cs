bf0583b5e0fd15da35de211fd8ef203e35d778b4
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:42:35 +0300

    Added sorters

---
+++ src/ModsenBookLibrary.Application/Sorters/BookSorter.cs
@@ -0,0 +1,17 @@
+﻿using ModsenBookLibrary.Application.Sorters.Base;
+using ModsenBookLibrary.Application.Sorters.Fields;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Sorters;
+internal class BookSorter : ISorter<Book, BookSortField>
+{
+    public IQueryable<Book> Sort(IQueryable<Book> models, BookSortField field, bool ascending)
+    {
+        return field switch
+        {
+            BookSortField.None => models,
+            BookSortField.Name => ascending ? models.OrderBy(x => x.Name) : models.OrderByDescending(x => x.Name),
+            _ => models
+        };
+    }
+}
---
+++ src/ModsenBookLibrary.Application/Sorters/Fields/BookSortField.cs
@@ -0,0 +1,6 @@
+﻿namespace ModsenBookLibrary.Application.Sorters.Fields;
+public enum BookSortField
+{
+    None,
+    Name
+}
