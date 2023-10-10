f6c64c1c3684a7d26fcd717002e1606cb3123698
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:37:13 +0300

    Created configurations

---
+++ src/ModsenBookLibrary.Infrastructure/Data/Configurations/GenreConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Infrastructure.Data.Configurations;
+
+internal class GenreConfiguration : IEntityTypeConfiguration<Genre>
+{
+    public void Configure(EntityTypeBuilder<Genre> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+    }
+}
---
+++ src/ModsenBookLibrary.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Infrastructure.Data.Configurations;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Name).IsRequired().HasMaxLength(25);
+    }
+}
