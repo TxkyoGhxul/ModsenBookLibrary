55b42d7496742149e3e9d9a9b23c38745d4a6301
pxmiu <pmnsplay@gmail.com>
2023-10-10 21:40:07 +0300

    Implemented base command handler

---
+++ src/ModsenBookLibrary.Application/Commands/BaseCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Domain.Models.Base;
+
+namespace ModsenBookLibrary.Application.Commands;
+internal abstract class BaseCommandHandler<TModel> where TModel : IEntity, new()
+{
+    protected readonly IRepository<TModel> _repository;
+    protected readonly IUnitOfWork _unitOfWork;
+
+    protected BaseCommandHandler(IRepository<TModel> repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+}
