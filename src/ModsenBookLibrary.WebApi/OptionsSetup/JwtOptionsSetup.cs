2db74c20c0fc1064aaca7adf1ce1f6e404b93468
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:26:03 +0300

    Setups for jwt tokens

---
+++ src/ModsenBookLibrary.WebApi/OptionsSetup/JwtOptionsSetup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using ModsenBookLibrary.Infrastructure.Auth;
+
+namespace ModsenBookLibrary.WebApi.OptionsSetup;
+
+public class JwtOptionsSetup : IConfigureOptions<JwtOptions>
+{
+    private const string _sectionName = "Jwt";
+
+    private readonly IConfiguration _configuration;
+
+    public JwtOptionsSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(JwtOptions options)
+    {
+        _configuration.GetSection(_sectionName).Bind(options);
+    }
+}
