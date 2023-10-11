67c46d30a786d28239a7ffa5e2d13b228c55c688
pxmiu <pmnsplay@gmail.com>
2023-10-11 19:58:05 +0300

    Helper for hashing the password

---
+++ src/ModsenBookLibrary.Application/Helpers/HashHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ModsenBookLibrary.Application.Helpers;
+internal static class HashHelper
+{
+    public static string GetPasswordHash(string password)
+    {
+        var byteString = Encoding.UTF8.GetBytes(password);
+
+        using var shaManager = SHA256.Create();
+        var byteHash = shaManager.ComputeHash(byteString);
+
+        var hash = BitConverter.ToString(byteHash).Replace("-", string.Empty);
+
+        return hash;
+    }
+}
