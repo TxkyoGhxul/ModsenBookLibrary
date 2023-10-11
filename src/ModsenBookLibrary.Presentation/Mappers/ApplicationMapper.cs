186cefe34f074f73829375f2bbe89d04d114c322
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:26:18 +0300

    Added application mapper

---
+++ src/ModsenBookLibrary.Presentation/Mappers/ApplicationMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ModsenBookLibrary.Domain.Models;
+using ModsenBookLibrary.Presentation.Dtos;
+
+namespace ModsenBookLibrary.Presentation.Mappers;
+internal class ApplicationMapper : Profile
+{
+    public ApplicationMapper()
+    {
+        CreateMap<Book, BookDto>();
+        CreateMap<BookDistribution, BookDistributionDto>();
+        CreateMap<Genre, GenreDto>();
+        CreateMap<Role, RoleDto>();
+        CreateMap<User, UserDto>();
+    }
+}
