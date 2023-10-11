3941f235d205faae60d87c91138bf0a7aa2eec5c
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:23:51 +0300

    Implemented get all genres query

---
+++ src/ModsenBookLibrary.Application/Queries/GenreQueries/GetRange/GetAllGenresQuery.cs
@@ -0,0 +1,5 @@
+﻿using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Queries.GenreQueries.GetRange;
+public record GetAllGenresQuery : IQuery<Genre>;
