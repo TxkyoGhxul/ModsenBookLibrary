de8e6e0b2a6790f944dd9b10adeb965e85513ca2
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:24:17 +0300

    Added roles constants

---
+++ src/ModsenBookLibrary.Domain/Constants/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace ModsenBookLibrary.Domain.Constants;
+public static class Roles
+{
+    public const string User = nameof(User);
+
+    public const string Admin = nameof(Admin);
+
+    public const string Author = nameof(Author);
+}
