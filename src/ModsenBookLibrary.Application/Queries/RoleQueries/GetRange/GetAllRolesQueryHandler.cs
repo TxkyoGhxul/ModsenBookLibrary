e4ae1f9d831982aa5900bc50285a6968d4c25ce9
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:24:00 +0300

    Implemented get all roles query

---
+++ src/ModsenBookLibrary.Application/Queries/RoleQueries/GetRange/GetAllRolesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Application.Extensions;
+using ModsenBookLibrary.Application.Models;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Queries.RoleQueries.GetRange;
+
+internal class GetAllRolesQueryHandler : BaseQueryHandler<Role>, IQueryHandler<GetAllRolesQuery, Role>
+{
+    public GetAllRolesQueryHandler(IRepository<Role> repository) : base(repository)
+    {
+    }
+
+    public async Task<Result<PagedList<Role>>> Handle(GetAllRolesQuery request, CancellationToken cancellationToken)
+    {
+        var roles = _repository.GetAll();
+        var countRoles = await roles.CountAsync(cancellationToken);
+
+        return await roles.ToPagedListAsync(1, countRoles, cancellationToken);
+    }
+}
