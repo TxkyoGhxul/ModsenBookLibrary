fdd15d5e3db12632be7e999071fcac8a627f7a6f
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:22:29 +0300

    Implemented create role command

---
+++ src/ModsenBookLibrary.Application/Commands/RoleCommands/Create/CreateRoleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Application.Models;
+using ModsenBookLibrary.Domain.Exceptions;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Commands.RoleCommands.Create;
+
+internal class CreateRoleCommandHandler : BaseCommandHandler<Role>, ICreateCommandHandler<CreateRoleCommand, Role>
+{
+    public CreateRoleCommandHandler(IRepository<Role> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+    {
+    }
+
+    public async Task<Result<Role>> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
+    {
+        var role = await _repository.Get(role => role.Name ==  request.Name).FirstOrDefaultAsync(cancellationToken);
+        if (role != null)
+        {
+            return new DublicateException();
+        }
+
+        var newRole = new Role(request.Name);
+        _repository.Create(newRole);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return newRole;
+    }
+}
---
+++ src/ModsenBookLibrary.Application/Commands/RoleCommands/Create/CreateRoleCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace ModsenBookLibrary.Application.Commands.RoleCommands.Create;
+
+internal class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+{
+    public CreateRoleCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().Length(3, 25);
+    }
+}
