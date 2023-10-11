98e7f80ddf24b25a98bb48b13120d01695d1e426
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:27:30 +0300

    Added lazy loading to ef

--- src/ModsenBookLibrary.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/ModsenBookLibrary.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Infrastructure.Auth;
 using ModsenBookLibrary.Infrastructure.Data;
 using ModsenBookLibrary.Infrastructure.Repositories;
 
@@ -14,6 +16,7 @@ public static class ServiceCollectionExtensions
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             options.UseSqlServer(connectionString);
+            options.UseLazyLoadingProxies();
         });
 
         services.AddScoped<IBookDbContext, BookDbContext>();
@@ -25,6 +28,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection InjectRepositories(this IServiceCollection services)
     {
         services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+        return services;
+    }
+
+    public static IServiceCollection InjectJwtBearer(this IServiceCollection services)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+
+        services.AddScoped<IJwtTokenProvider, JwtTokenProvider>();
 
         return services;
     }
