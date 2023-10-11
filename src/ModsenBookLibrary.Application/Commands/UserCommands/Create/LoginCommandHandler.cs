7600ce5fe9e87c340f11e386c85f0afd8d363cb5
pxmiu <pmnsplay@gmail.com>
2023-10-11 17:22:41 +0300

    Implemented user auth commands

---
+++ src/ModsenBookLibrary.Application/Commands/UserCommands/Create/LoginCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModsenBookLibrary.Application.Interfaces;
+using ModsenBookLibrary.Application.Models;
+using ModsenBookLibrary.Domain.Exceptions;
+using ModsenBookLibrary.Domain.Models;
+
+namespace ModsenBookLibrary.Application.Commands.UserCommands.Create;
+
+internal class LoginCommandHandler : BaseCommandHandler<User>, ICreateCommandHandler<LoginCommand, string>
+{
+    private readonly IJwtTokenProvider _tokenProvider;
+
+    public LoginCommandHandler(IJwtTokenProvider tokenProvider, IRepository<User> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+    {
+        _tokenProvider = tokenProvider;
+    }
+
+    public async Task<Result<string>> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _repository
+            .Get(u => u.Email == request.Email && u.Password == request.Password)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (user == null)
+        {
+            return new EntityNotFoundException("User not found. Check login and password");
+        }
+
+        var token = _tokenProvider.GetJwtToken(user);
+
+        return Result<string>.From(token);
+    }
+}
---
+++ src/ModsenBookLibrary.Application/Commands/UserCommands/Create/LoginCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ModsenBookLibrary.Application.Commands.UserCommands.Create;
+
+internal class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(c =>  c.Email).Must(email => !string.IsNullOrWhiteSpace(email));
+
+        RuleFor(c =>  c.Password).Must(password => !string.IsNullOrWhiteSpace(password));
+    }
+}
